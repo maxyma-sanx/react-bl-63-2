591207f577298de69ac43e1cb21c9e37f62e5223
KP-Svitlana <kovalenko.svitlana.p@gmail.com>
2023-02-11 21:56:55 +0200

    task_@

--- src/tabs/Gallery.jsx
+++ src/tabs/Gallery.jsx
@@ -2,12 +2,17 @@ import { Component } from 'react';
 
 import * as ImageService from 'service/image-service';
 import { Button, SearchForm, Grid, GridItem, Text, CardItem } from 'components';
+import { Loader } from '../components/Loader/Loader';
 
 export class Gallery extends Component {
   state = {
     query: '',
     page: 1,
     photos: [],
+    isEmpty: false,
+    showBtn: false,
+    error: '',
+    isLoading: false,
   };
 
   async componentDidUpdate(_, prevState) {
@@ -15,23 +20,44 @@ export class Gallery extends Component {
 
     if (query !== prevState.query || page !== prevState.page) {
       try {
+        this.setState({ isLoading: true });
         const { photos, total_results } = await ImageService.getImages(
           query,
           page
         );
-
+        if (!photos.length) {
+          this.setState({ isEmpty: true });
+          return;
+        }
         this.setState(prevState => ({
           photos: [...prevState.photos, ...photos],
+          showBtn: page < Math.ceil(total_results / 15),
         }));
-      } catch (error) {}
+      } catch (error) {
+        this.setState({ error: error.message });
+      } finally {
+        this.setState({ isLoading: false });
+      }
     }
   }
 
   handleSubmit = query => {
-    this.setState({ query });
+    this.setState({
+      query,
+      page: 1,
+      photos: [],
+      isEmpty: false,
+      showBtn: false,
+      error: '',
+    });
   };
+
+  onLoadMoreBtnClick = () => {
+    this.setState(prevState => ({ page: prevState.page + 1 }));
+  };
+
   render() {
-    const { photos } = this.state;
+    const { photos, isEmpty, showBtn, error, isLoading } = this.state;
     return (
       <>
         <SearchForm onSubmit={this.handleSubmit} />
@@ -46,7 +72,16 @@ export class Gallery extends Component {
             );
           })}
         </Grid>
-        <Text textAlign="center">Sorry. There are no images ... 😭</Text>
+        {isEmpty && (
+          <Text textAlign="center">Sorry. There are no images ... 😭</Text>
+        )}
+        {showBtn && (
+          <Button onClick={this.onLoadMoreBtnClick}>Load more...</Button>
+        )}
+        {error && (
+          <Text textAlign="center">"Sorry something going wrong..."😭</Text>
+        )}
+        {isLoading && <Loader />}
       </>
     );
   }
