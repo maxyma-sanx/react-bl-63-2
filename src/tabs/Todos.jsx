ae0db69f41be5a1818fd941edead02e8a9ea85f1
Anton Bobryshev <101340267+BobryshevAnton@users.noreply.github.com>
2023-02-11 21:27:19 +0100

    add

--- src/tabs/Todos.jsx
+++ src/tabs/Todos.jsx
@@ -3,7 +3,32 @@ import { nanoid } from 'nanoid';
 import { Grid, GridItem, SearchForm, EditForm, Text, Todo } from 'components';
 
 export class Todos extends Component {
+  state = {
+    todos: [],
+  };
+
+  handleSubmit = text => {
+    const todo = {
+      text,
+      id: nanoid(),
+    };
+    this.setState(prevState => ({ todos: [...prevState.todos, todo] }));
+  };
+
   render() {
-    return <Text>Todos</Text>;
+    return (
+      <>
+        <SearchForm onSubmit={this.handleSubmit} />
+        <Grid>
+          {this.state.todos.map((elem, index) => {
+            return (
+              <GridItem key={elem.id}>
+                <Todo text={elem.text} counter={index + 1} />
+              </GridItem>
+            );
+          })}
+        </Grid>
+      </>
+    );
   }
 }
